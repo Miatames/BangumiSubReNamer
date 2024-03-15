cbc7de07d4f652dfdd9b033e34c8771a57449915
Miatames <mesotamia1@outlook.com>
2024-03-15 23:08:49 +0800

    调整默认输出路径

--- BangumiSubReNamer/ViewModels/Pages/SettingsViewModel.cs
+++ BangumiSubReNamer/ViewModels/Pages/SettingsViewModel.cs
@@ -27,11 +27,7 @@ namespace BangumiSubReNamer.ViewModels.Pages
             {
                 OutFilePath += @"\";
             }
-            else if (OutFilePath.StartsWith(@"{RootPath}\"))
-            {
-                OutFilePath.Replace(@"{RootPath}\", @"{RootPath}");
-            }
-
+            
             var reNamerConfig = new DataReNamerConfig(
                 addSubFileExtensionRegex: AddSubFileExtensionRegex,
                 addSourceFileExtensionRegex: AddSourceFileExtensionRegex,
